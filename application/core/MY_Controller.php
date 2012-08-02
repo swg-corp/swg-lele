372ba432a4db4851b98e73f99ada52f3f53a960d
satriaprayoga <satria.prayoga@gmail.com>
2012-08-02 21:05:14 +0700

    initial branch commit

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MY_Controller
+ *
+ * @author satriaprayoga
+ */
+class MY_Controller extends CI_Controller{
+    
+    function __construct() {
+        parent::__construct();
+    }
+}
